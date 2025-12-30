ab1e4d9c0c52eb474050edb81eb7d29415fdecc7
Andrew Stake <andrewms98@live.com>
2025-12-30 14:42:54 -0500

    Added basic Meteor spawning

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -1,6 +1,11 @@
 extends Node2D
 
-var meteor_scene: PackedScene = load("res://Scenes/meteor.tscn"l)
+#1. Load the Scene
+var meteor_scene: PackedScene = load("res://Scenes/meteor.tscn")
 
 func _on_meteor_timer_timeout() -> void:
-	pass
+	#2. create an instance
+	var meteor = meteor_scene.instantiate()
+	
+	#3. attach the node to the scene tree
+	$Meteors.add_child(meteor)
--- Scenes/meteor.gd
+++ Scenes/meteor.gd
@@ -1,6 +1,18 @@
 extends Area2D
 
-
+func _ready():
+	#create the random number generator
+	var rng := RandomNumberGenerator.new()
+	
+	#start position
+	var width = get_viewport().get_visible_rect().size[0]
+	var height = get_viewport().get_visible_rect().size[1]
+	var randomX = rng.randi_range(0, width)
+	var randomY = rng.randi_range(-150,-50)
+	position = Vector2(randomX, randomY)
+	
+func _process(delta: float) -> void:
+	position += Vector2(0, 1.0) * 400 * delta
 
 func _on_body_entered(body: Node2D) -> void:
 	print('body entered')
