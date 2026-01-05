cccd6e4da854a7bc42b41974eda474e759068200
Andrew Stake <andrewms98@live.com>
2026-01-04 21:40:04 -0500

    Added stars and laser animation

--- Scenes/laser.gd
+++ Scenes/laser.gd
@@ -2,6 +2,11 @@ extends Area2D
 
 @export var speed = 600
 
+func _ready() -> void:
+	#$LaserSprite.scale = Vector2(0,0)
+	var tween = create_tween()
+	tween.tween_property($LaserSprite, 'scale', Vector2(1,1), 0.1).from(Vector2(0,0))
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.y -= speed * delta
--- Scenes/level.gd
+++ Scenes/level.gd
@@ -4,6 +4,22 @@ extends Node2D
 var meteor_scene: PackedScene = load("res://Scenes/meteor.tscn")
 var laser_scene: PackedScene = load("res://Scenes/laser.tscn")
 
+func _ready() -> void:
+	#stars
+	var size = get_viewport().get_visible_rect().size
+	var rng = RandomNumberGenerator.new()
+	for star in $Stars.get_children():
+		#position
+		var randomX = rng.randi_range(0, size.x)
+		var randomY = rng.randi_range(0, size.y)
+		star.position = Vector2(randomX,randomY)
+		#scale
+		var randomScale = rng.randf_range(0.35,0.65)
+		star.scale = Vector2(randomScale, randomScale)
+		#speed
+		var randomSpeed = rng.randf_range(0.8,1.2)
+		star.speed_scale = randomSpeed
+
 func _on_meteor_timer_timeout() -> void:
 	#2. create an instance
 	var meteor = meteor_scene.instantiate()
